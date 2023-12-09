8ea4ec6bd4247f1ea22399c711b773b6a24ca5e1
iradbouzidi <b.irad1994@gmail.com>
2023-12-09 16:48:23 +0100

    update

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,7 +1,6 @@
 import { g, config, auth } from "@grafbase/sdk";
 
-// @ts-ignore
-const User = g
+const user = g
   .model("User", {
     name: g.string().length({ min: 2, max: 100 }),
     email: g.string().unique(),
@@ -18,8 +17,7 @@ const User = g
     rules.public().read();
   });
 
-// @ts-ignore
-const Project = g
+const project = g
   .model("Project", {
     title: g.string().length({ min: 3 }),
     description: g.string(),
